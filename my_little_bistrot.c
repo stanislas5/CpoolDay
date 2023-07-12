972da6cc85a6cc1953a2fe9f18221e7ba15285b3
stanislas5 <139329508+stanislas5@users.noreply.github.com>
2023-07-12 13:42:57 +0000

    [ADD]my_little_bistrot.c

---
+++ my_little_bistrot.c
@@ -0,0 +1,14 @@
+int my_little_bistrot(int value1, char op, int value2);
+{
+    if op=+ {value1+value2}
+    else{
+        if op=- {value1-value2}
+            else {
+                if op=* {value1*value2}
+                    else{
+                        value1/value2
+                    }
+                }
+        }
+
+}
