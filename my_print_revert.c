6559073e732e62d10ced64b47fe5103ed9d03d39
stanislas5 <139329508+stanislas5@users.noreply.github.com>
2023-07-12 13:03:50 +0000

    [ADD]my_print_revert.c

---
+++ my_print_revert.c
@@ -0,0 +1,25 @@
+int compteL (char *str)
+{
+    int (i=0);
+    for ( ; str [i] = NULL; i++){
+        ;
+        }
+    return (i);
+}
+
+
+void my_print_revert (char* toRevert)
+{
+    x=compteL (toRevert)
+    for (; toRevert[x])]>=0; x-- ){
+        print_char(toRevert[x]);
+    }
+}   
+
+
+
+
+int main (void)
+{
+   my_print_n_ascii(90) ;
+}
