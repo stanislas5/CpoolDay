60f9dfc4d4083b612ce6c016f9c201ff9c63b07a
stanislas5 <139329508+stanislas5@users.noreply.github.com>
2023-07-12 09:40:10 +0000

    [ADD] my_print_n_ascii

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -1,6 +1,6 @@
 void my_print_n_ascii (int howMany)
 {
-    if (howMany<0 || howMany>93)
+    if (howMany<=0 || howMany>93)
         return;
     else {
         for (char x = '!' ; x <=howMany+33 ; x++ ) {
